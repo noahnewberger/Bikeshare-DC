0a8574961a86acb3ef4501e54b2bfd8ddd0aedff
Unknown <msussman@gmail.com>
2018-03-27 20:11:15 -0400

    Update SQL code for Travis

--- code_snippets/cabi_rides.py
+++ code_snippets/cabi_rides.py
@@ -1,33 +1,34 @@
 import psycopg2
 import pandas as pd
 import os
-from pathlib import Path
-from dotenv import load_dotenv
+import util_functions as uf
 import numpy as np
 import matplotlib.style as style
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-env_path = Path() / '.env'
-load_dotenv(dotenv_path=env_path)
-
-host = "capstone-bikeshare.cs9te7lm3pt2.us-east-1.rds.amazonaws.com"
-port = 5432
-database = "bikeshare"
-
-user = os.environ.get("AWS_READONLY_USER")
-password = os.environ.get("AWS_READONLY_PASS")
-
-# Connect to aws postgres DB
-conn = psycopg2.connect(host=host, user=user, port=port, password=password, database=database)
-cur = conn.cursor()
+uf.set_env_path()
+conn, cur = uf.aws_connect()
 
 # Query cabi trips
-cabi_trips = pd.read_sql("""SELECT duration/60000 as duration_mins, EXTRACT(YEAR FROM start_date) AS year, member_type from cabi_trips LIMIT 10""", con=conn)
+cabi_trips = pd.read_sql("""SELECT DISTINCT EXTRACT(YEAR FROM start_date) AS year,
+                                            member_type,
+                                            COUNT(*) as trip_count,
+                                            MIN(duration/60000) AS min_duration_mins,
+                                            AVG(duration/60000) AS mean_duration_mins,
+                                            MAX(duration/60000) AS max_duration_mins
+                                            from cabi_trips
+                                            WHERE member_type != 'Unknown' and EXTRACT(YEAR FROM start_date) = 2010
+                                            GROUP BY EXTRACT(YEAR FROM start_date), member_type
+                                            """, con=conn)
+
+print(cabi_trips)
+import sys
+sys.exit()
 
 #Calculate median and IQR
 duration_med = np.percentile(cabi_trips['duration_mins'], 50)
-q75, q25 = np.percentile(cabi_trips['duration_mins'], [75 ,25])
+q75, q25 = np.percentile(cabi_trips['duration_mins'], [75 , 25])
 iqr = q75 - q25
 
 #Remove values (outliers) that are 3 times IQR away from median
