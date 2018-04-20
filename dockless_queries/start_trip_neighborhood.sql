d895cdf23aab2c06cace60559bef51b803185ec5
Unknown <msussman@gmail.com>
2018-04-20 17:11:13 -0400

    Updated dockless_queries

---
+++ dockless_queries/start_trip_neighborhood.sql
@@ -0,0 +1,12 @@
+﻿SELECT * FROM crosstab($$
+select distinct 
+nbh_names, 
+operatorclean,
+count(*) as total_rides
+from dockless_trips as d
+left join dockless_ngh_lookup n_start
+on d.startlongitude = n_start.lon and d.startlatitude = n_start.lat
+GROUP BY 1, 2
+ORDER BY 1, 2$$
+,$$SELECT unnest('{jump,lime,mobike,ofo,spin}'::text[])$$)
+AS ct ("neighborhood" text, "jump" int, "lime" int, "mobike" int, "ofo" int, "spin" int);
