7cd80965788f7f0322c7f6b332a516824f434afd
Mark Sussman <msussman@mathematica-mpr.com>
2018-05-29 09:30:44 -0400

    Clean up CaBi Trips Loading scripts

--- data_loads/load_cabi_trips.py
+++ data_loads/load_cabi_trips.py
@@ -1,8 +1,41 @@
 import pandas as pd
 import os
 import util_functions as uf
+from splinter import Browser
+import urllib.request
+import shutil
+import zipfile
 
-# TODO: Define function to pull CaBi Trip data from DDOT source
+
+def get_links():
+    browser = Browser('firefox')
+    browser.visit('https://s3.amazonaws.com/capitalbikeshare-data/index.html')
+
+    links = browser.find_by_css('#tbody-content a')
+    #links = [link for link in links if link['href']]
+    zip_links = []
+    for link in links:
+        link_text = link['href']
+        with urllib.request.urlopen(link_text) as response:
+            subtype = response.info().get_content_subtype()
+            if subtype == 'zip':
+                zip_links.append(link_text)
+
+    return zip_links
+
+
+def download_links(links):
+    download_dir = '../cabi_trip_data'
+    if not os.path.exists(download_dir):
+        os.mkdir(download_dir)
+    for link in links:
+        file_name = os.path.join(download_dir, link.split('/')[-1])
+        print(file_name)
+        # save file to disk
+        with urllib.request.urlopen(link) as response, open(file_name, 'wb') as out_file:
+            shutil.copyfileobj(response, out_file)
+        with zipfile.ZipFile(file_name, "r") as zip_ref:
+            zip_ref.extractall(download_dir)
 
 
 def trips_to_df(cabi_trip_dir):
@@ -34,6 +67,9 @@ def create_cabi_trips(cur):
 
 
 if __name__ == "__main__":
+    # Pull CaBi Trips Data from AWS S3 Store
+    links = get_links()
+    download_links(links)
     # Connect to AWS
     uf.set_env_path()
     conn, cur = uf.aws_connect()
